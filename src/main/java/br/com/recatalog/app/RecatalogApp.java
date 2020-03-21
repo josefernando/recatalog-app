64295928fe89a8ff1c1e9d9c83752cdfd2aae395
José Fernando Pereira <jose.ze.fernando@gmail.com>
2020-03-21 09:25:20 -0300

    spring project ready

---
+++ src/main/java/br/com/recatalog/app/RecatalogApp.java
@@ -0,0 +1,12 @@
+package br.com.recatalog.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecatalogApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecatalogApp.class, args);
+	}
+}
