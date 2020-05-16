72da22c47c41a21c156718c841421e096615cc3b
José Fernando Pereira <jose.ze.fernando@gmail.com>
2020-05-15 21:31:21 -0300

    integration thymeleaf-spring-security

--- src/main/java/br/com/recatalog/app/controller/LoginController.java
+++ src/main/java/br/com/recatalog/app/controller/LoginController.java
@@ -4,16 +4,26 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class LoginController {
 	@PostMapping("login")
 	public String login() {
-		return "index.html";
+		return "login.html";
 	}	
 	
+	/*
+	 * No caso de falha do login, automaticamente será chamado o request GET "/login"
+	 *  Ref.: https://docs.spring.io/spring-security/site/docs/current/reference/pdf/spring-security-reference.pdf
+	 *  pag. 62
+	 */
+	
 	@GetMapping("login")
-	public String loginx() {
-		return "index.html";
+	public ModelAndView loginx() {
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("login.html");
+		mav.addObject("login", true);
+		return mav;
 	}
 }
